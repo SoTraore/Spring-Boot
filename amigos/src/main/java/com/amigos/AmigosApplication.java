2c114347f49ab8d8ac0579d877bdaa40f1b64182
SoTraore <Souleymane.TRAORE@etu.uca.fr>
2024-02-06 23:22:24 +0100

    Initial commit

---
+++ amigos/src/main/java/com/amigos/AmigosApplication.java
@@ -0,0 +1,15 @@
+package com.amigos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigosApplication.class, args);
+	}
+
+}
+
+
---
+++ amigos/src/main/java/com/amigos/GreetingControler.java
@@ -0,0 +1,20 @@
+package com.amigos;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+class GreetingController {
+
+    @GetMapping("/")
+    public GreetResponse greet() {
+        Person p = new Person("Alex", 28, 30_000); // Added missing semicolon
+        GreetResponse res = new GreetResponse("Hello", List.of("Java", "JS", "Golang", "Swift"), p);
+        return res;
+    }
+
+    record GreetResponse(String greet, List<String> lists, Person person) {} // Corrected record syntax
+    record Person(String name, int age, double savings) {}
+}
